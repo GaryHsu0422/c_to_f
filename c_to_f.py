75d01e8cc6fd69a59f00afb632fbc8c75ce9f28a
Gary Hsu <garyhsu0422@gmaill.com>
2021-08-18 11:02:11 +0800

    celcius to fahrenheit

---
+++ c_to_f.py
@@ -0,0 +1,6 @@
+celcius = input ('What is the celcius temperature today: ')
+celcius = int(celcius)
+fahrenheit = celcius * 9 / 5 + 32
+fahrenheit = float(fahrenheit)
+print ('the fahrenheit temperature is: ', fahrenheit )
+
